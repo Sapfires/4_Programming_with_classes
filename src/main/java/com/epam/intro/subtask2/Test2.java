ac4b238fe7afbe15ae97325ad9135eb36e9814eb
Sapfires <nastya-zykov@tut.by>
2021-04-27 23:06:23 +0300

    SubTask2

---
+++ src/main/java/com/epam/intro/subtask2/Test2.java
@@ -0,0 +1,32 @@
+package com.epam.intro.subtask2;
+
+public class Test2 {
+    private int one;
+    private int two;
+
+    public Test2(int one, int two) {
+        this.one = one;
+        this.two = two;
+    }
+
+    public Test2() {
+        this.one = 3;
+        this.two = 18;
+    }
+
+    public int getOne() {
+        return one;
+    }
+
+    public void setOne(int one) {
+        this.one = one;
+    }
+
+    public int getTwo() {
+        return two;
+    }
+
+    public void setTwo(int two) {
+        this.two = two;
+    }
+}
